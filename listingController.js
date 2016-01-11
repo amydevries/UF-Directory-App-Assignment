c7c375087d11504be7eca2c592b7b5ac47d41859
Kyle Brost <kjbrost@gmail.com>
2016-01-11 16:23:02 -0500

    added some functionality to showDetails function

--- listingController.js
+++ listingController.js
@@ -1,11 +1,11 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
     $scope.addListing = function() {
       /*
@@ -15,14 +15,22 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
 
     $scope.deleteListing = function(index) {
       /*
-      removing something from the todo list 
+      removing something from the todo list
       $scope.todos.splice(index,1);
       */
     };
 
     $scope.showDetails = function(index) {
-      $scope.details = $scope.listings[index];
-      console.log($scope.selected);
+        var address = $scope.listings[index].address,
+            lat = $scope.listings[index].coordinates.latitude,
+            long = $scope.listings[index].coordinates.longitude;
+        if(typeof lat === 'undefined' || typeof long === 'undefined')
+        {
+            $scope.detailedinfo = address;
+        }
+        else {
+          $scope.detailedInfo = address + ' @' + lat + ', ' + long;
+        }
     };
 
   }
