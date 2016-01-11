f7fd83fe1496f5bedfbc4a21887d02343a8dad19
Amy <amy.devries@ufl.edu>
2016-01-11 15:07:24 -0500

    Work in progress
    Added delete functionality.
    Working on side panel to display detailed information.

--- listingController.js
+++ listingController.js
@@ -18,11 +18,22 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       removing something from the todo list 
       $scope.todos.splice(index,1);
       */
+      $scope.listings.splice(index,1);
     };
 
+    $scope.showMe = false; 
+    $scope.myFunc = function(){
+      $scope.showMe = !$scope.showMe;
+    }
+
     $scope.showDetails = function(index) {
-      $scope.details = $scope.listings[index];
+      /*$scope.details = $scope.listings[index];
       console.log($scope.selected);
+      */
+      $scope.detailedInfo = index; 
+      console.log(index);
+      console.log($scope.detailedInfo);
+      $scope.showMe = true;
     };
 
   }
