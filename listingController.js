ce8b88a9699ae87f0494e8b2836fe99a8f16acd0
Amy <amy.devries@ufl.edu>
2016-01-11 12:29:23 -0500

    Listings and filter
    Added code to populate listings and filter search.

--- listingController.js
+++ listingController.js
@@ -7,8 +7,23 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      /*
+      $scope.todos.push(newItem);
+      */
+    };
+
+    $scope.deleteListing = function(index) {
+      /*
+      removing something from the todo list 
+      $scope.todos.splice(index,1);
+      */
+    };
+
+    $scope.showDetails = function(index) {
+      $scope.details = $scope.listings[index];
+      console.log($scope.selected);
+    };
+
   }
 ]);
